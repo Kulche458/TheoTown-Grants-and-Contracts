5d46716f44272150665601ac9f065a6f68e21f45
Kulche <kulche458@gmail.com>
2026-05-06 17:26:32 +0200

    Balance rewards, fix security issue

--- lua/storage.lua
+++ lua/storage.lua
@@ -1,11 +1,13 @@
 
 -- This module handles data storage and serialization. Contract data should preferably persist between sessions.
 
+local Identifier = require('identifier')
+
 local Storage = {}
 
 -- Initialize an load stored data.
 function Storage.init()
-    local s = Util.optStorage(City.getStorage(), '$kulche_contracts_script_storage_00')
+    local s = Util.optStorage(City.getStorage(), Identifier.get())
 
     s.contracts = s.contracts or {
         active = {},
